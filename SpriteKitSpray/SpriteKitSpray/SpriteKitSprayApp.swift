a7eaf52a0a94ba44ca60b7b23cd0c994de24b9ba
Furkan Hanci <60580341+Furkanus@users.noreply.github.com>
2020-10-20 15:40:18 +0300

    Add files via upload

---
+++ SpriteKitSpray/SpriteKitSpray/SpriteKitSprayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitSprayApp.swift
+//  SpriteKitSpray
+//
+//  Created by Furkan Hanci on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitSprayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
